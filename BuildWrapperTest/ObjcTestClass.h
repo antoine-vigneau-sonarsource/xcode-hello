3cdb772b89bd6c87d0b93e324601b232ab3370f2
Antoine Vigneau <antoine.vigneau@sonarsource.com>
2021-09-03 15:57:18 +0200

    Initial commit

---
+++ BuildWrapperTest/ObjcTestClass.h
@@ -0,0 +1,15 @@
+//
+//  ObjcTestClass.h
+//  BuildWrapperTest
+//
+//  Created by Michael Heinzl on 16.10.20.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ObjcTestClass : NSObject
+
+- (int)test;
+- (void)notImplmented;
+
+@end
---
+++ BuildWrapperTest/ObjcTestClass.m
@@ -0,0 +1,16 @@
+//
+//  ObjcTestClass.m
+//  BuildWrapperTest
+//
+//  Created by Michael Heinzl on 16.10.20.
+//
+
+#import "ObjcTestClass.h"
+
+@implementation ObjcTestClass
+
+- (int)test {
+	return 10;
+}
+
+@end
